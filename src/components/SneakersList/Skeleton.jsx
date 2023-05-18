37885b11b81924da1bda91880af47f5bae0ffd86
ODyachenko <oleg1696@gmail.com>
2023-05-18 12:03:12 +0300

    fixed Skeleton on mobile

--- src/components/SneakersList/Skeleton.jsx
+++ src/components/SneakersList/Skeleton.jsx
@@ -11,13 +11,13 @@ const Skeleton = (props) => (
     foregroundColor="#f2f2f2"
     {...props}
   >
-    <rect x="30" y="0" rx="10" ry="10" width="150" height="90" />
-    <rect x="30" y="105" rx="3" ry="3" width="150" height="15" />
-    <rect x="28" y="388" rx="8" ry="8" width="98" height="40" />
+    <rect x="0" y="0" rx="10" ry="10" width="210" height="90" />
+    <rect x="0" y="105" rx="3" ry="3" width="210" height="15" />
+    <rect x="0" y="388" rx="8" ry="8" width="110" height="40" />
     <rect x="238" y="388" rx="8" ry="8" width="98" height="40" />
-    <rect x="30" y="125" rx="3" ry="3" width="95" height="15" />
-    <rect x="30" y="228" rx="8" ry="8" width="80" height="25" />
-    <rect x="148" y="224" rx="8" ry="8" width="32" height="32" />
+    <rect x="0" y="125" rx="3" ry="3" width="110" height="15" />
+    <rect x="0" y="228" rx="8" ry="8" width="80" height="25" />
+    <rect x="170" y="224" rx="8" ry="8" width="32" height="32" />
   </ContentLoader>
 );
 
